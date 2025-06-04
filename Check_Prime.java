7d88ffb91d5fbea0c058db9ed40958a9cdac31b5
Geetesh <geetesh472@gmail.com>
2025-06-04 14:19:25 +0530

    Check Prime

--- Check_Prime.java
+++ Check_Prime.java
@@ -4,6 +4,7 @@ public class Check_Prime {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
+        sc.close();
         if(num == 2) {
             System.out.println("Prime");
             return;
