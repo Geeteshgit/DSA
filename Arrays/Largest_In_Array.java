379245e27ef057d626645249d9eaa8f6c8992ed2
Geetesh <geetesh472@gmail.com>
2025-06-05 23:36:53 +0530

    Largest Number in Array

--- Arrays/Largest_In_Array.java
+++ Arrays/Largest_In_Array.java
@@ -5,7 +5,7 @@ public class Largest_In_Array {
         int nums[] = { 1, 2, 6, 3, 5, 8, 15, 7, 20, 9 };
         int len = nums.length;
         int max = nums[0];
-        for(int i = 0; i < len; i++) {
+        for(int i = 1; i < len; i++) {
             if(nums[i] > max) {
                 max = nums[i];
             }
