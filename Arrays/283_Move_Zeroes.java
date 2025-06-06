410729afd4791506fa67970bf515e14db51c4888
Geetesh <geetesh472@gmail.com>
2025-06-06 23:27:58 +0530

    283. Move Zeroes

--- Arrays/283_Move_Zeroes.java
+++ Arrays/283_Move_Zeroes.java
@@ -1,19 +1,20 @@
 package Arrays;
 
 class Solution {
-    public void moveZeroes(int[] a) {
-        int n=a.length,j=0;
-        for(int i=0;i<n;i++){
-            if(a[i]!=0){
-                swap(a,i,j);
-                j++;
-
+    public void moveZeroes(int[] nums) {
+        int pos = 0;
+        int count = 0;
+        int len = nums.length;
+        for(int i = 0; i < len; i++) {
+            if(nums[i] == 0) count++;
+            if(nums[i] != 0) {
+                nums[pos] = nums[i];
+                pos++;
             }
         }
-    }
-    public void swap(int[] a,int i,int j){
-        int t=a[i];
-        a[i]=a[j];
-        a[j]=t;
+        for(int i = len - count; i < len ; i++) {
+            nums[i] = 0;
+        }
+        return;
     }
 }
