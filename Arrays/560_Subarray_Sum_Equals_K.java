8dd7f2c1ad91d9a63c232cbd0b51941b43e01c11
Geetesh <geetesh472@gmail.com>
2025-06-09 17:05:36 +0530

    560. Subarray Sum Equals K

---
+++ Arrays/560_Subarray_Sum_Equals_K.java
@@ -0,0 +1,27 @@
+package Arrays;
+
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        int len = nums.length;
+        int prefix[] = new int[len];
+        int count = 0;
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        prefix[0] = nums[0];
+        for(int i = 1; i < len; i++) {
+            prefix[i] = prefix[i-1] + nums[i];
+        }
+        for(int j = 0; j < len; j++) {
+            if(prefix[j] == k) count++;
+            int val = prefix[j] - k;
+            if(hm.containsKey(val)) {
+                count += hm.get(val);
+            }
+            if(!hm.containsKey(prefix[j])) {
+                hm.put(prefix[j], 0);
+            }
+            int freq = hm.get(prefix[j]);
+            hm.put(prefix[j], freq + 1);
+        }
+        return count;
+    }
+}
