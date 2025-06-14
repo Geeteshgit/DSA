f1a4e57bf7562bbf937e2431ab6a239f1cd4f4f3
Geetesh <geetesh472@gmail.com>
2025-06-14 21:39:51 +0530

    443. String Compression

---
+++ Strings/443_String_Compression.java
@@ -0,0 +1,27 @@
+package Strings;
+import java.util.*;
+
+class Solution {
+    public int compress(char[] chars) {
+        int len = chars.length;
+        if(len == 1) return 1;
+        StringBuilder sb = new StringBuilder("");
+        
+        for(int i = 0; i < len; i++) {
+           int count = 1;
+           while(i < len - 1 && chars[i] == chars[i+1]) {
+            count++;
+            i++;
+           }
+           if(count == 1) sb.append(chars[i]);
+           else {
+            sb.append(chars[i]);
+            sb.append(count);
+           }
+        }
+        for(int i = 0; i < sb.length(); i++) {
+            chars[i] = sb.charAt(i);
+        }
+        return sb.length();
+    }
+}
