83bf09496c52aff86d4cb51fe4c0a1065aa658a4
Geetesh <geetesh472@gmail.com>
2025-06-15 20:05:50 +0530

    35. Search Insert Position

--- Binary_Search/35_Search_Insert_Position.java
+++ Binary_Search/35_Search_Insert_Position.java
@@ -13,7 +13,7 @@ class Solution {
                 high = mid - 1;
             }
             else {
-                low = mid + 1;a
+                low = mid + 1;
             }
         }
         return low;
