ada4edc30e84965e7c213ae27f1c97c375d7b3d2
Geetesh <geetesh472@gmail.com>
2025-07-02 01:56:12 +0530

    Clear Bit

--- Bit_Manipulation/Clear_Bit.java
+++ Bit_Manipulation/Clear_Bit.java
@@ -4,7 +4,7 @@ public class Clear_Bit {
     public static void main(String[] args) {
         int num = 13;
         int i = 2;
-        int newNum = num & (~(1 << i));
+        int newNum = num & (~ (1 << i));
         System.out.println(newNum);
     }   
 }
