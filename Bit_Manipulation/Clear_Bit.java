4cf7663979a4a412beac1b0693ffcd83d692b46d
Geetesh <geetesh472@gmail.com>
2025-07-02 01:55:38 +0530

    Cleat Bit

---
+++ Bit_Manipulation/Clear_Bit.java
@@ -0,0 +1,10 @@
+package Bit_Manipulation;
+
+public class Clear_Bit {
+    public static void main(String[] args) {
+        int num = 13;
+        int i = 2;
+        int newNum = num & (~(1 << i));
+        System.out.println(newNum);
+    }   
+}
