410e8060254018a33fd872f4ba75fe8309e0a2b7
Geetesh <geetesh472@gmail.com>
2025-07-08 04:58:36 +0530

    42. Trapping Rainwater

--- Arrays/42_Trapping_Rain_Water.java
+++ Arrays/42_Trapping_Rain_Water.java
@@ -2,21 +2,31 @@ package Arrays;
 
 class Solution {
     public int trap(int[] height) {
-        int len = height.length;
-        int[] leftMax = new int[len];
-        int[] rightMax = new int[len];
-        leftMax[0] = height[0];
+        int low = 0;
+        int high = height.length - 1;
+        int maxLeft = 0;
+        int maxRight = 0;
         int trappedWater = 0;
-        for(int i = 1; i < len; i++) {
-            leftMax[i] = Math.max(height[i], leftMax[i-1]);
-        }
-        rightMax[len-1] = height[len-1];
-        for(int i = len-2; i >= 0; i--) {
-            rightMax[i] = Math.max(height[i], rightMax[i+1]);
-        }
-        for(int i = 0; i < len; i++) {
-            int level = Math.min(leftMax[i], rightMax[i]);
-            trappedWater += level - height[i];
+        if(height.length <= 2) return 0;
+        while (low <= high) {
+            if(maxLeft < maxRight) {
+                if(height[low] > maxLeft) {
+                    maxLeft = height[low];
+                }
+                else {
+                    trappedWater += maxLeft - height[low];
+                }
+                low++;
+            }
+            else {
+                if(maxRight < height[high]) {
+                    maxRight = height[high];
+                }
+                else {
+                    trappedWater += maxRight - height[high];
+                }
+                high--;
+            }
         }
         return trappedWater;
     }
