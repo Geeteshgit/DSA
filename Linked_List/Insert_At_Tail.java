b5a4191c5cf3dfce21fbe342331c7f3ada4f08a9
Geetesh <geetesh472@gmail.com>
2025-07-07 22:56:24 +0530

    Insert new Node at Tail

---
+++ Linked_List/Insert_At_Tail.java
@@ -0,0 +1,40 @@
+package Linked_List;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int data, Node next) {
+        this.data = data;
+        this.next = next;
+    }
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class Insert_At_Tail {
+    public static void main(String[] args) {
+        int arr[] = { 1,2,3,4,5 };
+        Node head = new Node(arr[0]);
+        Node curr = head;
+        for(int i = 1; i < arr.length; i++) {
+            Node temp = new Node(arr[i]);
+            curr.next = temp;
+            curr = curr.next;
+        }
+        Node currNode = head;
+        while(currNode.next != null) {
+            currNode = currNode.next;
+        }
+        Node newNode = new Node(6);
+        currNode.next = newNode;
+        Node tcurr = head;
+        while(tcurr != null) {
+            System.out.print(tcurr.data + " -> ");
+            tcurr = tcurr.next;
+        }
+        System.out.println("null");
+    }
+}
