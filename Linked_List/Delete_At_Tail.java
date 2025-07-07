843368a328b42f9523db14ec299cc0f538f2376f
Geetesh <geetesh472@gmail.com>
2025-07-07 23:07:15 +0530

    Delete Node at Tail

---
+++ Linked_List/Delete_At_Tail.java
@@ -0,0 +1,39 @@
+package Linked_List;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int data, Node next) {
+        this.data = data;
+        this.next = next;
+    }
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class Delete_At_Tail {
+    public static void main(String[] args) {
+        int arr[] = { 1,2,3,4,5 };
+        Node head = new Node(arr[0]);
+        Node curr = head;
+        for(int i = 1; i < arr.length; i++) {
+            Node temp = new Node(arr[i]);
+            curr.next = temp;
+            curr = curr.next;
+        }
+        Node currNode = head;
+        while(currNode.next.next != null) {
+            currNode = currNode.next;
+        }
+        currNode.next = null;
+        Node tcurr = head;
+        while(tcurr != null) {
+            System.out.print(tcurr.data + " -> ");
+            tcurr = tcurr.next;
+        }
+        System.out.println("null");
+    }
+}
